fc02851409919549d3b1e3182915199df4c03adc
Jakub Kalinowski <jakub.kalinowski@developx.com>
2025-10-05 19:55:53 +0200

    Update site URL and sitemap in configuration, remove old index page, and create a new index page with a "Coming Soon" message and basic layout. Enhance styles for better presentation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://diana-kalinowska-prawnik.pl',
+  site: 'https://kancelariakalinowska.pl',
   integrations: [
     sitemap(),
     tailwind({
